569e623f7a3fdc48efd8166bd165723db8bdb1ad
lizirukh <lizirukh@gmail.com>
2023-02-05 02:57:18 +0400

    New Project

---
+++ src/components/Landing.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Landing.css";
+import Logo from "../assets/logo.png";
+import Logo2 from "../assets/logo2.png";
+
+const Landing = () => {
+  const addHandler = () => {
+    alert("Successful!");
+  }
+
+  return (
+    <div className="landing">
+      <img src={Logo} alt="" className="logo" />
+      <hr className="hr"/>
+      {/* <img src={Logo2} alt="" className="logo2" /> */}
+      <button className="button" onClick={addHandler}>რეზიუმეს დამატება</button>
+    </div>
+  );
+};
+
+export default Landing;
