fb2b553acb5b8457e8a857f44264651668e727a9
lizirukh <lizirukh@gmail.com>
2023-02-11 17:09:56 +0400

    Updated Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,10 @@ const Header = (title, pageNum) => {
         <button className="btn">
           <img src={Vector} alt="" />
         </button>
-        <div className="title">პირადი ინფო</div>
-        <text className="pageNum">1/3</text>
+        <div className="cont1">
+          <div className="title1">პირადი ინფო</div>
+          <div className="pageNum">1/3</div>
+        </div>
       </div>
       <hr className="line" />
     </div>
