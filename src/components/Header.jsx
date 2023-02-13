7cd5998497c0467c1e8b0c3e61ff89eec2ab1fb8
lizirukh <lizirukh@gmail.com>
2023-02-13 23:43:42 +0400

    Updated Header and made back button reactive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Header.css";
 import Vector from "../assets/Vector.png";
+import Landing from "./Landing";
 
 const Header = (title, pageNum) => {
+  const [active, setActive] =useState(false);
+
+  if (active) {
+    return <Landing/>
+  }
+
   return (
     <div className="header">
       <div className="container">
-        <button className="btn">
+        <button className="btn" onClick={() => {setActive(true)}}>
           <img src={Vector} alt="" />
         </button>
         <div className="cont1">
-          <div className="title1">პირადი ინფო</div>
+          <div className="title1">ᲞᲘᲠᲐᲓᲘ ᲘᲜᲤᲝ</div>
           <div className="pageNum">1/3</div>
         </div>
       </div>
