9b5753dec57c6547143da6edeccd150650dc6765
lizirukh <lizirukh@gmail.com>
2023-02-13 23:46:55 +0400

    Made button reactive

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Landing.css";
 import Logo from "../assets/logo.png";
+import InfoPage from "./InfoPage";
 // import Logo2 from "../assets/logo2.png";
+import { Navigate } from 'react-router-dom';
 
 const Landing = () => {
-  const addHandler = () => {
-    alert("Successful!");
+  const [active, setActive] =useState(false);
+
+  if (active) {
+    return <InfoPage/>
   }
 
   return (
     <div className="landing">
       <img src={Logo} alt="" className="logo" />
       <hr className="hr"/>
-      <button className="button" onClick={addHandler}>რეზიუმეს დამატება</button>
+      <button className="button" onClick={() => {setActive(true)}}>ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ</button>
     </div>
   );
 };
